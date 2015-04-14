f67a828d0b855423e60be30d77a3afef074d77b1
Syed Humza Shah <humzashah@gmail.com>
2015-04-14 20:20:02 +0200

    randomized test order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'convenient_grouper'
 
 RSpec.configure do |config|
+  # order tests:
+  config.order = "random"
+
+  # format output:
   config.color = true
   config.formatter = :documentation
 end
