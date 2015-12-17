730f37e4c53d90f04a7983f379bbbae9b386315b
Syed Humza Shah <humzashah@gmail.com>
2015-12-17 10:23:13 +0000

    moved .rspec settings to spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'convenient_grouper'
+
+RSpec.configure do |config|
+  config.color = true
+  config.formatter = :documentation
+  config.order = :random
+end
